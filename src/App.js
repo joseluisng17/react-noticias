ea0df661e88431e45c41e8322732c0389d71c618
jose Luis NG <joseluisng17@gmail.com>
2020-07-12 14:35:42 -0500

    subiendo el proyecto primera parte

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
+import Error from './components/Error';
+
 
 function App() {
+
+  // definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+  const [error, guardarError] = useState(false);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=7cdd3ae556cd4c4aa34e4254eb8ccb80`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      //console.log(noticias)
+      guardarNoticias(noticias.articles);
+
+       // Detecta si hubo resultados correctos en la consulta
+
+      if(noticias.totalResults === 0) {
+        guardarError(true);
+      } else {
+        guardarError(false);
+      }
+
+    }
+    consultarAPI();
+  }, [categoria]);
+
+  let componenteListadoNoticias;
+  if(error) {
+    componenteListadoNoticias = <Error mensaje="No hay resultados" />
+  } else {
+    componenteListadoNoticias = <ListadoNoticias 
+                  noticias={noticias}
+                />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+        <Header 
+          titulo='Buscador de Noticias'
+        />
+
+        <div className="container white">
+            <Formulario 
+              guardarCategoria={guardarCategoria}
+            />
+
+            {componenteListadoNoticias}
+        </div>
+    </Fragment>
   );
 }
 
